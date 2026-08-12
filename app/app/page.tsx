bd3f3c8d526e9fa136764d8b251d6666d02359cc
spennybern <someshitonline@protonmail.ch>
2026-08-12 09:36:40 +0700

    Add Home component with SpencerWidget

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+import SpencerWidget from './components/SpencerWidget';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-slate-900 to-slate-800">
+      <SpencerWidget />
+    </main>
+  );
+}
